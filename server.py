8d4e580435612bbbe6d949eefe8ca3f77d64e230
Компуктер <Raven9880@mail.ru>
2023-03-17 00:33:43 +0300

    добавление видео сервера

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 import socket
 import threading
 
-HOST = '192.168.0.14'
+HOST = '192.168.0.12'
 PORT = 9000
-
 
 class Server:
     def __init__(self):
@@ -22,12 +21,11 @@ class Server:
             client, address = self.server.accept()
             print(f'{address[0]} подключился к серверу!')
             for any_client in self.all_client:
-                any_client.send(f'{address[0]} вошел в чат!'.encode('utf-8'))
+                any_client.send(f'{address[0]} вошел в чат!\n'.encode('utf-8'))
             if client not in self.all_client:
                 self.all_client.append(client)
-                threading.Thread(target=self.message_handler, args=(client, address)).start()
-                client.send('Успешное подключение к чату!'.encode('utf-8'))
-                threading.Thread(target=self.video_handler, args=(client,)).start()
+            threading.Thread(target=self.message_handler, args=(client, address)).start()
+            client.send('Успешное подключение к чату!\n'.encode('utf-8'))
 
     def message_handler(self, client_socket, address):
         while True:
@@ -41,17 +39,7 @@ class Server:
                 self.all_client.remove(client_socket)
                 client_socket.close()
                 for any_client in self.all_client:
-                    any_client.send(f'{address[0]} покинул чат!'.encode('utf-8'))
-                break
-
-    def video_handler(self, client_socket):
-        while True:
-            try:
-                video_signal = client_socket.recv(1024)
-                for client in self.all_client:
-                    if client != client_socket:
-                        client.send(video_signal)
-            except:
+                    any_client.send(f'{address[0]} покинул чат!\n'.encode('utf-8'))
                 break
 
 
