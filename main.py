c0363a0cf5a78a2496ba2d087ed91425afe9a965
Roman <Raven9880@mail.ru>
2023-03-22 23:59:46 +0300

    изменен транспортный протокол для перенапровления видео сигнала через UDP, рефакторинг всего кода, добавлены окна при нажатии кнопок во вкладках: техника, ремонты, персонал

--- main.py
+++ main.py
@@ -1,87 +1,108 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
 import socket
 import sys
 import time
 import cv2
-import pickle
-import struct
-from PyQt5 import QtWidgets, QtCore, QtGui
+import numpy as np
 
 from login_win import Ui_Login
 from main_win import Ui_MW
+from add_pers_win import Ui_Form as add_pers_win
+from add_repair_win import Ui_Form as add_repair_win
+from add_tec_win import Ui_Form as add_tec_win
+from add_work_part_win import Ui_Dialog as add_work_part_win
 
-login_pwd = ("111", "222")
-HOST = '192.168.0.13'
+LOGIN_PSWRD = {'1': '111', '2': '222', '3': '333'}
+# SERVER_IP = '192.168.194.125'
+MAIN_IP = socket.gethostname()
 PORT = 9000
+VIDEO_PORT = 9001
 
 
 class MessageMonitor(QtCore.QThread):
+    """Класс реализован потоком для отслеживания чата приложения"""
     mysignal = QtCore.pyqtSignal(str)
 
     def __init__(self, server_socket, parent=None):
+        """Инициализация"""
         QtCore.QThread.__init__(self, parent)
         self.server_socket = server_socket
         self.message = None
 
     def run(self):
+        """Запуск потока"""
         while True:
             self.message = self.server_socket.recv(1024)
             self.mysignal.emit(self.message.decode('utf-8'))
 
-class data_ac(QtCore.QThread):
+
+class VideoHandler(QtCore.QThread):
+    """Класс реализован потоком для отслеживания перенаправленных
+    сервером видеопотоков и отоброжения"""
     mysignal = QtCore.pyqtSignal(QtGui.QImage)
-    def __init__(self, server_socket, parent=None):
+
+    def __init__(self, parent=None):
+        """Инициализация"""
         QtCore.QThread.__init__(self, parent)
-        self.server_socket = server_socket
+        self.vid_handler = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.vid_handler.bind((socket.gethostbyname(MAIN_IP), VIDEO_PORT))
 
     def run(self):
-        while True:
-            try:
-                data = b''
-                payload_size = struct.calcsize("Q")
-                while True:
-                    while len(data) < payload_size:
-                        packet = self.server_socket.recv(1024 * 4)
-                        if not packet:
-                            break
-                        data += packet
-                    packed_msg_size = data[:payload_size]
-                    data = data[payload_size:]
-                    msg_size = struct.unpack("Q", packed_msg_size)[0]
-                    while len(data) < msg_size:
-                        data += self.server_socket.recv(1024 * 4)
-                    frame_data = data[:msg_size]
-                    data = data[msg_size:]
-                    frame = pickle.loads(frame_data)
-                    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    flipped_image = cv2.flip(image, 1)
-                    convert_to_qt_format = QtGui.QImage(flipped_image.data, flipped_image.shape[1],
-                                                flipped_image.shape[0], QtGui.QImage.Format_RGB888)
-                    pic = convert_to_qt_format.scaled(640, 480, QtCore.Qt.KeepAspectRatio)
-                    self.mysignal.emit(pic)
-            except:
-                self.server_socket.close()
+        """Запуск потока"""
+        try:
+            while True:
+                data, _ = self.vid_handler.recvfrom(65507)
+                nparr = np.frombuffer(data, np.uint8)
+                img_decode = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                image = cv2.cvtColor(img_decode, cv2.COLOR_BGR2RGB)
+                flipped_image = cv2.flip(image, 1)
+                conv_to_qt_format = QtGui.QImage(flipped_image.data, flipped_image.shape[1],
+                                                 flipped_image.shape[0], QtGui.QImage.Format_RGB888)
+                pic = conv_to_qt_format.scaled(640, 480, QtCore.Qt.KeepAspectRatio)
+                self.mysignal.emit(pic)
+        except:
+            print('Ошибка с перенапровлением видеопотока')
+
 
 class VideoMonitor(QtCore.QThread):
-    def __init__(self, server_socket, parent=None):
+    """Класс реализован потоком для оброботки сигнала с веб-камеры и его отоброжения,
+    так-же для отправки видеопотока на сервер для последующего перенапровления клиентам"""
+    mysignal = QtCore.pyqtSignal(QtGui.QImage)
+
+    def __init__(self, parent=None):
+        """Инициализация"""
         QtCore.QThread.__init__(self, parent)
-        self.server_socket = server_socket
+        self.vid_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.vid = cv2.VideoCapture(0)
 
     def run(self):
         while True:
             try:
-                while True:
-                    ret, frame = self.vid.read()
-                    a = pickle.dumps(frame)
-                    msg = struct.pack("Q", len(a)) + a
-                    self.server_socket.sendall(msg)
+                ret, frame = self.vid.read()
+                if ret:
+                    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    flipped_img = cv2.flip(image, 1)
+                    conv_to_qt_frm = QtGui.QImage(flipped_img.data, flipped_img.shape[1],
+                                                  flipped_img.shape[0], QtGui.QImage.Format_RGB888)
+                    pic = conv_to_qt_frm.scaled(640, 480, QtCore.Qt.KeepAspectRatio)
+                    self.mysignal.emit(pic)
+
+                    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+                    result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+                    data = np.array(imgencode)
+                    string_data = data.tobytes()
+                    self.vid_client.sendto(string_data, (SERVER_IP, VIDEO_PORT))
+
             except:
-                self.server_socket.close()
+                print('Ошибка с оброботки сигнала с веб-камеры')
+                self.vid_client.close()
                 self.vid.release()
 
 
 class LoginWin(Ui_Login):
+    """Запуск окна входа в приложения с последующим отоброжение главного окна"""
     def __init__(self):
+        """Инициализация"""
         app = QtWidgets.QApplication(sys.argv)
         self.login_win = QtWidgets.QMainWindow()
         self.ui = Ui_Login()
@@ -92,43 +113,69 @@ class LoginWin(Ui_Login):
         self.start_w = Ui_MW()
         self.start_w.setupUi(self.main_win)
         self.start_w.pushButton.clicked.connect(self.input_msg)
+        self.start_w.pushButton_4.clicked.connect(self.add_tec_win)
+        self.start_w.pushButton_9.clicked.connect(self.add_pers_win)
+        self.start_w.pushButton_6.clicked.connect(self.add_rep_win)
+        self.tcp_client = None
+        self.anim = None
+        self.message_monitor = None
+        self.video_monitor = None
+        self.video_handler = None
 
         sys.exit(app.exec_())
 
     def push_btn(self):
-        self.tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.tcp_client.connect((HOST, PORT))
+        """Функция для отрисобки главного окна.
+        Создание потоков реализована в данной функие"""
+        try:
+            if self.ui.lineEdit.text() in LOGIN_PSWRD and \
+                    LOGIN_PSWRD[self.ui.lineEdit.text()] == self.ui.lineEdit_2.text():
+                self.tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.tcp_client.connect((SERVER_IP, PORT))
 
-        self.vid_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.vid_client.connect((HOST, 9001))
+            if self.ui.lineEdit.text() in LOGIN_PSWRD and \
+                    LOGIN_PSWRD[self.ui.lineEdit.text()] == self.ui.lineEdit_2.text():
+                self.login_win.close()
+                self.main_win.show()
+                self.anim = QtCore.QPropertyAnimation(self.main_win, b"pos")
+                self.anim.setEasingCurve(QtCore.QEasingCurve.InOutCubic)
+                self.anim.setStartValue(QtCore.QPoint(0, 0))
+                self.anim.setEndValue(QtCore.QPoint(300, 100))
+                self.anim.setDuration(1000)
+                self.anim.start()
 
-        self.message_monitor = MessageMonitor(self.tcp_client)
-        self.message_monitor.mysignal.connect(self.update_chat)
-        self.message_monitor.start()
+                self.message_monitor = MessageMonitor(self.tcp_client)
+                self.message_monitor.mysignal.connect(self.update_chat)
+                self.message_monitor.start()
 
-        self.video_monitor = VideoMonitor(self.vid_client)
-        self.video_monitor.start()
+                self.video_monitor = VideoMonitor()
+                self.video_monitor.mysignal.connect(self.image_update_slot)
+                self.video_monitor.start()
 
-        self.data_ac = data_ac(self.vid_client)
-        self.data_ac.mysignal.connect(self.image_update_slot)
-        self.data_ac.start()
+                self.video_handler = VideoHandler()
+                self.video_handler.mysignal.connect(self.image_update_slot2)
+                self.video_handler.start()
 
-        if self.ui.lineEdit.text() and self.ui.lineEdit_2.text() in login_pwd:
-            self.login_win.close()
-            self.main_win.show()
-            self.anim = QtCore.QPropertyAnimation(self.main_win, b"pos")
-            self.anim.setEasingCurve(QtCore.QEasingCurve.InOutCubic)
-            self.anim.setStartValue(QtCore.QPoint(0, 0))
-            self.anim.setEndValue(QtCore.QPoint(300, 100))
-            self.anim.setDuration(1000)
-            self.anim.start()
+            else:
+                dlg = QtWidgets.QDialog()
+                dlg.setWindowTitle("Ошибка входа")
+                dlg.setFixedSize(180, 70)
+                dlg.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
+                label = QtWidgets.QLabel("Неверный логин или пароль")
+                layout = QtWidgets.QVBoxLayout()
+                btn = QtWidgets.QPushButton("Ok", dlg)
+                btn.clicked.connect(dlg.close)
+                layout.addWidget(label)
+                layout.addWidget(btn)
+                dlg.setLayout(layout)
+                dlg.exec_()
 
-        else:
+        except (ConnectionRefusedError, TimeoutError):
             dlg = QtWidgets.QDialog()
             dlg.setWindowTitle("Ошибка входа")
             dlg.setFixedSize(180, 70)
             dlg.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)
-            label = QtWidgets.QLabel("Неверный логин или пароль")
+            label = QtWidgets.QLabel("Сервер недоступен")
             layout = QtWidgets.QVBoxLayout()
             btn = QtWidgets.QPushButton("Ok", dlg)
             btn.clicked.connect(dlg.close)
@@ -138,12 +185,19 @@ class LoginWin(Ui_Login):
             dlg.exec_()
 
     def update_chat(self, value):
+        """Функция для обновления чата от других клиентов"""
         self.start_w.textBrowser.append(value)
 
     def image_update_slot(self, image):
-        self.start_w.graphicsView.setPixmap(QtGui.QPixmap.fromImage(image))
+        """Функция для обновления видео с веб-камер"""
+        self.start_w.camera_label2.setPixmap(QtGui.QPixmap.fromImage(image))
+
+    def image_update_slot2(self, image):
+        """Функция для обновления видео с веб-камер других клиентов"""
+        self.start_w.camera_label.setPixmap(QtGui.QPixmap.fromImage(image))
 
     def input_msg(self):
+        """Функция для обновления чата и отправки данных другим клиентам"""
         message = self.start_w.textEdit.toPlainText()
         if len(message) > 0:
             self.start_w.textEdit.clear()
@@ -153,6 +207,32 @@ class LoginWin(Ui_Login):
             self.start_w.textBrowser.append(send_msg)
             self.tcp_client.send(send_msg.encode('utf-8'))
 
+    def add_tec_win(self):
+        self.added_win = QtWidgets.QWidget()
+        self.ui = add_tec_win()
+        self.ui.setupUi(self.added_win)
+        self.added_win.show()
+
+    def add_rep_win(self):
+        self.added_win = QtWidgets.QWidget()
+        self.ui = add_repair_win()
+        self.ui.setupUi(self.added_win)
+        self.added_win.show()
+        self.ui.pushButton_5.clicked.connect(self.add_work_win)
+
+    def add_pers_win(self):
+        self.added_win = QtWidgets.QWidget()
+        self.ui = add_pers_win()
+        self.ui.setupUi(self.added_win)
+        self.added_win.show()
+
+    def add_work_win(self):
+        self.added_win = QtWidgets.QDialog()
+        self.ui = add_work_part_win()
+        self.ui.setupUi(self.added_win)
+        self.added_win.show()
+
 
 if __name__ == "__main__":
+    SERVER_IP = input('Введите ип адрес сервера: ')
     LoginWin()
--- server.py
+++ server.py
@@ -1,42 +1,47 @@
+"""
+
+****SERVER WTCS****
+made by bannikovrt@list.ru
+
+"""
 import socket
 import threading
 
-HOST = '192.168.0.13'
-PORT = 9000
-VIDEO_PORT = 9001
+HOST = socket.gethostname()
+
 
 class Server:
-    def __init__(self):
-        self.ip = HOST
-        self.port = PORT
-        self.video_port = VIDEO_PORT
+    """Данный класс является основном для запуска сервера
+    в него передаются параметры портов, на которых будет запущен сервер"""
+    def __init__(self, ip=socket.gethostbyname(HOST), port=9000, video_port=9001):
+        """Инициализация"""
+        self.ip = ip
+        self.port = port
+        self.video_port = video_port
         self.all_client = []
-        self.all_video_client = []
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.ip, self.port))
         self.server.listen()
-        self.video_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.video_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.video_server.bind((self.ip, self.video_port))
-        self.video_server.listen()
         threading.Thread(target=self.connect_handler).start()
         print('Сервер запущен')
 
     def connect_handler(self):
+        """Функция для установки контакта с клиентами"""
         while True:
             client, address = self.server.accept()
-            video_client, video_address = self.video_server.accept()
             print(f'{address[0]} подключился к серверу!')
             for any_client in self.all_client:
                 any_client.send(f'{address[0]} вошел в чат!\n'.encode('utf-8'))
-            if video_client not in self.all_video_client:
-                self.all_video_client.append(video_client)
             if client not in self.all_client:
                 self.all_client.append(client)
                 client.send('Успешное подключение к чату!\n'.encode('utf-8'))
                 threading.Thread(target=self.message_handler, args=(client, address)).start()
-                threading.Thread(target=self.video_handler, args=(video_client,)).start()
+                threading.Thread(target=self.video_handler).start()
 
     def message_handler(self, client_socket, address):
+        """Функция для обработки сообщений чата приложения"""
         while True:
             try:
                 message = client_socket.recv(1024)
@@ -51,17 +56,17 @@ class Server:
                     any_client.send(f'{address[0]} покинул чат!\n'.encode('utf-8'))
                 break
 
-    def video_handler(self, client_socket):
+    def video_handler(self):
+        """Функция для приема видеопотока и перенапровление его клиентам"""
         while True:
             try:
-                data = client_socket.recv(4024)
-                for client in self.all_video_client:
-                    if client == client_socket:
-                        client.send(data)
+                data, address = self.video_server.recvfrom(65507)
+                for client in self.all_client:
+                    self.video_server.sendto(data, (client.getsockname()[0], 9001))
             except:
-                self.all_video_client.remove(client_socket)
-                client_socket.close()
-                break
+                print('Ошибка с перенапровлением видеосигнала')
 
 
-Server()
+if __name__ == '__main__':
+    print(socket.gethostbyname(HOST))
+    Server()
